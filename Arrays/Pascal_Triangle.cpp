3509a4a4efbb41e9905354fd7ef45fbe79146efe
jeevan jot singh <jeevanjotsingh0168@gmail.com>
2025-07-12 21:09:39 +0530

    Added Solution for Pascal Triangle

---
+++ Arrays/Pascal_Triangle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> triangle;
+
+    for (int i = 0; i < numRows; i++) {
+        vector<int> row(i + 1, 1);
+
+        for (int j = 1; j < i; j++) {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+
+        triangle.push_back(row);
+    }
+
+    return triangle;
+}
+
+int main() {
+    int n = 5;
+    vector<vector<int>> result = generate(n);
+
+    for (auto row : result) {
+        for (int num : row) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
