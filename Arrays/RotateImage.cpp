f8c207d4515e2c66f754ba8188b00f754bc8e972
jeevan jot singh <jeevanjotsingh0168@gmail.com>
2025-07-14 22:12:59 +0530

    Added Solution for Rotate Image

---
+++ Arrays/RotateImage.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    // Step 1: Transpose
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    vector<vector<int>> matrix = {{1, 2, 3},
+                                  {4, 5, 6},
+                                  {7, 8, 9}};
+
+    rotate(matrix);
+
+    cout << "Rotated matrix:\n";
+    for (auto row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
